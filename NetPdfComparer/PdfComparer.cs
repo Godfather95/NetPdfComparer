75473ade2c9edac3a66ed5cff983fea6500670de
Dennis Thieme <dennis.thieme95@googlemail.com>
2019-11-27 00:07:30 +0100

    First checkin

---
+++ NetPdfComparer/PdfComparer.cs
@@ -0,0 +1,62 @@
+﻿using Spire.Pdf;
+using System.Drawing;
+
+namespace NetPdfComparer
+{
+    public class PdfComparer
+    {
+        public static bool ComparePdf(string FileName1, string FileName2, out int ErrorPixels, out int CorrectPixels, out Bitmap CompareMask)
+        {
+            // temp variables
+            string PixelOriginal;
+            string PixelToCompare;
+            ErrorPixels = 0;
+            CorrectPixels = 0;
+            bool IsSamePdf = true;
+            int Counter = 0;
+
+            // Create Bitmaps
+            Bitmap BitmapOriginal = ReadBitmapFromPdf(FileName1);
+            Bitmap BitmapToCompare = ReadBitmapFromPdf(FileName2);
+            CompareMask = new Bitmap(BitmapOriginal.Width, BitmapOriginal.Height);
+
+            // Compare Size
+            if (BitmapOriginal.Width == BitmapToCompare.Width && BitmapOriginal.Height == BitmapToCompare.Height)
+            {
+                // Loop pixels - x
+                for (int i = 0; i < BitmapOriginal.Width; i++)
+                {
+                    // Loop pixels - y
+                    for (int j = 0; j < BitmapOriginal.Height; j++)
+                    {
+                        PixelOriginal = BitmapOriginal.GetPixel(i, j).ToString();
+                        PixelToCompare = BitmapToCompare.GetPixel(i, j).ToString();
+                        // Compare Pixel by Pixel
+                        if (PixelOriginal != PixelToCompare)
+                        {
+                            // Write into comparrison mask
+                            CompareMask.SetPixel(i, j, Color.Red);
+                            ErrorPixels++;
+                            IsSamePdf = false;
+                        }
+                        else
+                        {
+                            // Write into comparrison mask
+                            CompareMask.SetPixel(i, j, Color.White);
+                            CorrectPixels++;
+                        }
+                        Counter++;
+                    }
+                }
+            }
+            return IsSamePdf;
+        }
+
+        private static Bitmap ReadBitmapFromPdf(string PdfFile)
+        {
+            PdfDocument pdfDocument = new PdfDocument();
+            pdfDocument.LoadFromFile(PdfFile);
+            return (Bitmap)pdfDocument.SaveAsImage(0);
+        }
+    }
+}
---
+++ NetPdfComparerExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using NetPdfComparer;
+using System;
+using System.Drawing;
+
+namespace NetPdfComparerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Result = PdfComparer.ComparePdf(args[0], args[1], out int nErrors, out int nCorrects, out Bitmap CompareMask);
+            if (Result)
+            {
+                Console.WriteLine("PDF-Files are equal!");
+            }
+            else
+            {
+                Console.WriteLine($"PDF-Files are not equal! Correct pixels: {nCorrects} | Incorrect pixels: {nErrors}");
+            }
+            CompareMask.Save(args[2]);
+            Console.WriteLine($"Comparison-Mask saved at {args[2]}");
+            Console.ReadLine();
+        }
+    }
+}
