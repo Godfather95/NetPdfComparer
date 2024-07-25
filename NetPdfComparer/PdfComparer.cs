def969a2f4808b75b026c3109748a2e08a3d28d1
Dennis Thieme <dennis.thieme95@googlemail.com>
2024-07-25 13:04:37 +0200

    Refactoring + Aktualisierung Abhängigkeiten

--- NetPdfComparer/PdfComparer.cs
+++ NetPdfComparer/PdfComparer.cs
@@ -2,61 +2,63 @@
 using System;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Versioning;
 
-namespace NetPdfComparer
+namespace NetPdfComparer;
+
+public static class PdfComparer
 {
-    public class PdfComparer
+    [SupportedOSPlatform("windows")]
+    public static bool ComparePdf(string FileName1, string FileName2, out int ErrorPixels, out int CorrectPixels, out Bitmap CompareMask)
     {
-        public static bool ComparePdf(string FileName1, string FileName2, out int ErrorPixels, out int CorrectPixels, out Bitmap CompareMask)
-        {
-            // temp variables
-            string PixelOriginal;
-            string PixelToCompare;
-            ErrorPixels = 0;
-            CorrectPixels = 0;
-            bool IsSamePdf = true;
-            int Counter = 0;
+        // temp variables
+        string PixelOriginal;
+        string PixelToCompare;
+        ErrorPixels = 0;
+        CorrectPixels = 0;
+        bool IsSamePdf = true;
+        int Counter = 0;
 
-            // Create Bitmaps
-            using Bitmap BitmapOriginal = ReadBitmapFromPdf(FileName1);
-            using Bitmap BitmapToCompare = ReadBitmapFromPdf(FileName2);
-            // Compare Size
-            if (BitmapOriginal.Width == BitmapToCompare.Width && BitmapOriginal.Height == BitmapToCompare.Height)
+        // Create Bitmaps
+        using Bitmap BitmapOriginal = ReadBitmapFromPdf(FileName1);
+        using Bitmap BitmapToCompare = ReadBitmapFromPdf(FileName2);
+        // Compare Size
+        if (BitmapOriginal.Width == BitmapToCompare.Width && BitmapOriginal.Height == BitmapToCompare.Height)
+        {
+            // Loop pixels - x
+            for (int i = 0; i < BitmapOriginal.Width; i++)
             {
-                // Loop pixels - x
-                for (int i = 0; i < BitmapOriginal.Width; i++)
+                // Loop pixels - y
+                for (int j = 0; j < BitmapOriginal.Height; j++)
                 {
-                    // Loop pixels - y
-                    for (int j = 0; j < BitmapOriginal.Height; j++)
+                    PixelOriginal = BitmapOriginal.GetPixel(i, j).ToString();
+                    PixelToCompare = BitmapToCompare.GetPixel(i, j).ToString();
+                    // Compare Pixel by Pixel
+                    if (PixelOriginal != PixelToCompare)
                     {
-                        PixelOriginal = BitmapOriginal.GetPixel(i, j).ToString();
-                        PixelToCompare = BitmapToCompare.GetPixel(i, j).ToString();
-                        // Compare Pixel by Pixel
-                        if (PixelOriginal != PixelToCompare)
-                        {
-                            // Write into comparrison mask
-                            BitmapOriginal.SetPixel(i, j, Color.Red);
-                            ErrorPixels++;
-                            IsSamePdf = false;
-                        }
-                        else
-                        {
-                            // Write into comparrison mask
-                            CorrectPixels++;
-                        }
-                        Counter++;
+                        // Write into comparrison mask
+                        BitmapOriginal.SetPixel(i, j, Color.Red);
+                        ErrorPixels++;
+                        IsSamePdf = false;
                     }
+                    else
+                    {
+                        // Write into comparrison mask
+                        CorrectPixels++;
+                    }
+                    Counter++;
                 }
             }
-            CompareMask = (Bitmap)BitmapOriginal.Clone();
-            return IsSamePdf;
         }
+        CompareMask = (Bitmap)BitmapOriginal.Clone();
+        return IsSamePdf;
+    }
 
-        private static Bitmap ReadBitmapFromPdf(string PdfFile)
-        {
-            using PdfDocument pdfDocument = new PdfDocument();
-            pdfDocument.LoadFromFile(PdfFile);
-            return (Bitmap)pdfDocument.SaveAsImage(0);
-        }
+    [SupportedOSPlatform("windows")]
+    private static Bitmap ReadBitmapFromPdf(string PdfFile)
+    {
+        using PdfDocument pdfDocument = new();
+        pdfDocument.LoadFromFile(PdfFile);
+        return (Bitmap)Image.FromStream(pdfDocument.SaveAsImage(0));
     }
 }
--- NetPdfComparerExample/Program.cs
+++ NetPdfComparerExample/Program.cs
@@ -3,44 +3,45 @@ using NetPdfComparer;
 using System;
 using System.Drawing;
 using System.IO;
+using System.Runtime.Versioning;
 
-namespace NetPdfComparerExample
+namespace NetPdfComparerExample;
+
+static class Program
 {
-    class Program
+    public class Options
     {
-        public class Options
-        {
-            [Option('o', "original", Required = false, HelpText = "Original PDF File")]
-            public string Original { get; set; }
+        [Option('o', "original", Required = false, HelpText = "Original PDF File")]
+        public string Original { get; set; }
 
-            [Option('c', "compare", Required = false, HelpText = "PDF to compare")]
-            public string Compare { get; set; }
+        [Option('c', "compare", Required = false, HelpText = "PDF to compare")]
+        public string Compare { get; set; }
 
-            [Option('m', "mask", Required = false, HelpText = "Path to output comparison mask")]
-            public string Mask { get; set; }
+        [Option('m', "mask", Required = false, HelpText = "Path to output comparison mask")]
+        public string Mask { get; set; }
 
-            [Option('r', "result", Required = false, HelpText = "parsable result file")]
-            public string Result { get; set; }
-        }
-
-        static void Main(string[] args) => _ = Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
-                                           {
-                                               bool Result = PdfComparer.ComparePdf(o.Original, o.Compare, out int nErrors, out int nCorrects, out Bitmap CompareMask);
-                                               string ResultFile = string.Empty;
-                                               if (Result)
-                                               {
-                                                   Console.WriteLine("PDF-Files are equal!");
-                                                   ResultFile += "{\"result\": \"equal\",";
-                                               }
-                                               else
-                                               {
-                                                   Console.WriteLine($"PDF-Files are not equal! Correct pixels: {nCorrects} | Incorrect pixels: {nErrors}");
-                                                   ResultFile += "{\"result\": \"not equal\",";
-                                               }
-                                               CompareMask.Save(o.Mask);
-                                               ResultFile += "\"mask\": \"" + o.Mask.Replace("\\", "\\\\") + "\"}";
-                                               Console.WriteLine($"Comparison-Mask saved at {o.Mask}");
-                                               File.WriteAllText(o.Result, ResultFile);
-                                           });
+        [Option('r', "result", Required = false, HelpText = "parsable result file")]
+        public string Result { get; set; }
     }
+
+    [SupportedOSPlatform("windows")]
+    static void Main(string[] args) => _ = Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
+                                       {
+                                           bool Result = PdfComparer.ComparePdf(o.Original, o.Compare, out int nErrors, out int nCorrects, out Bitmap CompareMask);
+                                           string ResultFile = string.Empty;
+                                           if (Result)
+                                           {
+                                               Console.WriteLine("PDF-Files are equal!");
+                                               ResultFile += "{\"result\": \"equal\",";
+                                           }
+                                           else
+                                           {
+                                               Console.WriteLine($"PDF-Files are not equal! Correct pixels: {nCorrects} | Incorrect pixels: {nErrors}");
+                                               ResultFile += "{\"result\": \"not equal\",";
+                                           }
+                                           CompareMask.Save(o.Mask);
+                                           ResultFile += "\"mask\": \"" + o.Mask.Replace("\\", "\\\\") + "\"}";
+                                           Console.WriteLine($"Comparison-Mask saved at {o.Mask}");
+                                           File.WriteAllText(o.Result, ResultFile);
+                                       });
 }
